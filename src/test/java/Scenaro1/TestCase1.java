85cf9b93568df090baad7200f0e98008ccf73007
Arpana <muriki.arpana@gmail.com>
2022-07-22 05:48:37 +0530

    Second Commit

---
+++ src/test/java/Scenaro1/TestCase1.java
@@ -0,0 +1,10 @@
+package Scenaro1;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Git and Github...");
+	}
+
+}
