73c75ac16fe5ebf5e4e69a8794410325ed0a7a4b
Arpana <muriki.arpana@gmail.com>
2022-07-22 05:45:46 +0530

    first commit

---
+++ src/main/java/proj01/GitD001/App.java
@@ -0,0 +1,13 @@
+package proj01.GitD001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
